17be4cc4df8e81b567f53048e4ecb65b1fec3704
vnext_mylt <mylt@vnext.vn>
2022-09-30 09:43:52 +0700

    first commit

---
+++ src/main/java/com/example/sample1/controller/UserController.java
@@ -0,0 +1,13 @@
+package com.example.sample1.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+	 @RequestMapping(value = "/test", method = RequestMethod.GET)
+	 public String test(){
+			return "123";
+	 }
+}
