faa4bd3d605530d17382642f232155b3e97d3612
Hữu Phát <55278454+huuphat99@users.noreply.github.com>
2022-10-03 00:02:17 +0700

    Update time exprire

--- src/main/java/com/example/sample1/constants/AnPhatConstants.java
+++ src/main/java/com/example/sample1/constants/AnPhatConstants.java
@@ -3,7 +3,7 @@ package com.example.sample1.constants;
 public class AnPhatConstants {
   public static final String BASE_URL_ADMIN = "/api/v1";
 
-  public static final long ACCESS_TOKEN_VALIDITY_SECONDS = 60;
+  public static final long ACCESS_TOKEN_VALIDITY_SECONDS = 60*60;
   public static final String TOKEN_PREFIX = "Bearer ";
   public static final String HEADER_STRING = "Authorization";
 }
